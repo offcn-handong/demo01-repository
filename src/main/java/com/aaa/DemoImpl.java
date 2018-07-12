f5c5caf8749333fe3f7fa2e74bd2c44bd7066f05
offcn <wangyi126hd@126.com>
2018-07-12 09:49:51 +0800

    添加demo001，修改demoimpl

--- src/main/java/com/aaa/DemoImpl.java
+++ src/main/java/com/aaa/DemoImpl.java
@@ -3,6 +3,7 @@ package com.aaa;
 public class DemoImpl implements Demo {
     @Override
     public void haha() {
+        System.out.println(111);
         System.out.println(123);
     }
 }
---
+++ src/main/java/com/aaa/demo111.java
@@ -0,0 +1,4 @@
+package com.aaa;
+
+public class demo111 {
+}
