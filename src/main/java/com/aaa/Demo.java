b9e31a7ad8067334024486f6706790c59ce054de
offcn <wangyi126hd@126.com>
2018-07-12 09:43:34 +0800

    项目第一次提交，提交了pom.xml ， web.xml

---
+++ src/main/java/com/aaa/Demo.java
@@ -0,0 +1,5 @@
+package com.aaa;
+
+public interface Demo {
+    public void haha();
+}
---
+++ src/main/java/com/aaa/DemoImpl.java
@@ -0,0 +1,8 @@
+package com.aaa;
+
+public class DemoImpl implements Demo {
+    @Override
+    public void haha() {
+        System.out.println(123);
+    }
+}
